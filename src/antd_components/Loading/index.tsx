bedf8727ed5e9f31b6fa3cc35d14123ad8f1bcb1
cavelanipedro <cavelanipedro@gmail.com>
2023-12-15 16:28:52 -0300

    #19 - add lottie loading

--- src/antd_components/Loading/index.tsx
+++ src/antd_components/Loading/index.tsx
@@ -1,10 +1,11 @@
 import * as S from './styles';
+import loadingAnimation from 'assets/loadingAnimation.json';
 
 function Loader() {
   return (
     <S.ScreenBlocker id="loader">
       <S.Content>
-        <S.Loader />
+        <S.LottieContainer animationData={loadingAnimation} />
         <S.Span>Carregando . . .</S.Span>
       </S.Content>
     </S.ScreenBlocker>
--- src/antd_components/Loading/styles.ts
+++ src/antd_components/Loading/styles.ts
@@ -1,4 +1,11 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+import Lottie from 'lottie-react';
+
+export const LottieContainer = styled(Lottie)`
+  margin-top: -40px;
+  width: 250px;
+  height: 250px;
+`;
 
 export const ScreenBlocker = styled.div`
   display: none;
@@ -23,23 +30,5 @@ export const Span = styled.span`
   color: #fff;
   font-size: 1.1em;
   margin-top: 15px;
-`;
-
-const Spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-export const Loader = styled.div`
-  border: 8px solid transparent;
-  border-radius: 50%;
-  border-top: 10px solid ${(props) => props.theme.colors.textPrimary};
-  border-bottom: 10px solid ${(props) => props.theme.colors.textPrimary};
-  width: 80px;
-  height: 80px;
-  animation: ${Spin} 1.5s linear infinite;
+  margin-top: -55px;
 `;
